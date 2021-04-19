7504ca4280ab3776c01a8d3d2dde0a72be40494d
Marlon ryzen <enginer26mj@gmail.com>
2021-04-18 23:43:37 -0500

    Testing

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 const { useState, useEffect, useRef } = React;
 
-function Grid({snake, food}) {
-  const grid = [];
-  let key = -1;
-
-  // Render Grid
-  for (let i = 0; i < 40; i++) {
-    const column = [];
-    for (let j = 0; j < 40; j++) {
-      column.push('Space');
+function Grid({snake, food, ints}) {
+  if (snake[0][0] > 39 || snake[0][0] < 0 || snake[0][1] > 39 || snake[0][1] < 0) {
+    return (
+      <h1 className="GameOver">Game Over!</h1>
+    )
+  } else {
+    const grid = [];
+    let key = -1;
+  
+    // Render Grid
+    for (let i = 0; i < 40; i++) {
+      const column = [];
+      for (let j = 0; j < 40; j++) {
+        column.push('Space');
+      }
+      grid.push(column);
     }
-    grid.push(column);
-  }
-
-  // Display Snake
-  for (let i = 0; i < snake.length; i++) {
-    grid[snake[i][1]].splice(snake[i][0], 1, 'Snake');
-  }
-  // Display Food
-  for (let i = 0; i < snake.length; i++) {
-    grid[food[0]].splice(food[1], 1, 'Food');
-  }
-
-  return (
-    grid.map((col) => {
-      return col.map((row) => {
-        key +=1
-        return <div key={key} className={row}></div>
+  
+    // Display Snake
+    for (let i = 0; i < snake.length; i++) {
+      grid[snake[i][1]].splice(snake[i][0], 1, 'Snake');
+    }
+    // Display Food
+    for (let i = 0; i < snake.length; i++) {
+      grid[food[0]].splice(food[1], 1, 'Food');
+    }
+  
+    return (
+      grid.map((col) => {
+        return col.map((row) => {
+          key +=1
+          return <div key={key} className={row}></div>
+        })
       })
-    })
-  )
+    )
+  }
 }
 
 function App() { 
-  const [gameOver, setGameOver] = useState(false);
   const [snake, setSnake] = useState(
     [
       [20, 20], 
@@ -43,7 +48,21 @@ function App() {
   );
   const [food, setFood] = useState([10, 15]);
   const [direction, setDirection] = useState('right');
-  const [gameStarted, setGameStarted] = useState(5000);
+  const [gameSpeed, setGameSpeed] = useState(5000);
+  const [score, setScore] = useState(0);
+
+  const reset = () => {
+    setSnake(
+      [
+        [20, 20], 
+        [19, 20], 
+        [18, 20]
+      ]
+    );
+    setDirection('right');
+    setGameSpeed(300);
+    setScore(0);
+  }
 
   function useInterval(callback, delay) {
     const savedCallback = useRef();
@@ -74,103 +93,114 @@ function App() {
     const { keyCode } = event;
     switch( keyCode ) {
       case 37:
-              setDirection('left');
-              break;
+        setDirection('left');
+        break;
       case 38:
-              setDirection('up');
-              break;                   
+        setDirection('up');
+        break;                   
       case 39:
-            setDirection('right');
-            break;
+        setDirection('right');
+        break;
       case 40:
-            setDirection('down');
-            break;
+        setDirection('down');
+        break;
       default:
-          break;            
-        }
+        break;            
+    }
   }
 
   const moveSnake = () => {
-    const newSnake = [...snake];
-    if ((newSnake[0][0] === food[1]) && (newSnake[0][1] === food[0])) {
-      const tail = newSnake[newSnake.length];
-      newSnake.push([tail]);
-      console.log(gameStarted);
-      setGameStarted(gameStarted - (gameStarted * 0.1));
-      console.log(gameStarted);
-      setFood([Math.floor(Math.random() * 40 ), Math.floor(Math.random() * 40 )]);
-    }
-    if (newSnake[0][0] > 39 || newSnake[0][0] < 0 || newSnake[0][1] > 39 || newSnake[0][1] < 0) {
-      setGameOver(true);
+    // Game Over
+    if (snake[0][0] > 39 || snake[0][0] < 0 || snake[0][1] > 39 || snake[0][1] < 0) {
+      return;
     } else {
-      if (direction === 'left') {
-        const head = [newSnake[0][0] - 1, newSnake[0][1]];
-        newSnake.unshift(head);
-        newSnake.pop();
-      } else if (direction === 'up') {
-        const head = [newSnake[0][0], newSnake[0][1] - 1];
-        newSnake.unshift(head);
-        newSnake.pop();
-      } else if (direction === 'right') {
-        const head = [newSnake[0][0] + 1, newSnake[0][1]];
-        newSnake.unshift(head);
-        newSnake.pop();
-      } else if (direction === 'down') {
-        const head = [newSnake[0][0], newSnake[0][1] + 1];
-        newSnake.unshift(head);
-        newSnake.pop();
+      const newSnake = [...snake];
+      // Grow Snake
+      if ((newSnake[0][0] === food[1]) && (newSnake[0][1] === food[0])) {
+        const tail = newSnake[newSnake.length];
+        setScore(c => c + 1);
+        newSnake.push([tail]);
+        if (gameSpeed > 40) {
+          setGameSpeed(gameSpeed - (gameSpeed * 0.1));
+        }
+        setFood([Math.floor(Math.random() * 40 ), Math.floor(Math.random() * 40 )]);
       }
-      setSnake(newSnake);
-    }
+      const head = [];
+      switch (direction) {
+        case 'left':
+          head.push(newSnake[0][0] - 1)
+          head.push(newSnake[0][1]);
+        case 'up':
+          head.push(newSnake[0][0])
+          head.push(newSnake[0][1] - 1);                   
+        case 'right':
+          head.push(newSnake[0][0] + 1)
+          head.push(newSnake[0][1]);
+        case 'down':
+          head.push(newSnake[0][0])
+          head.push(newSnake[0][1] + 1);
+        default:
+          newSnake.unshift(head);
+          newSnake.pop();
+          setSnake(newSnake);
+          break;            
+      }
+    }     
   }
 
-  useInterval(moveSnake, gameStarted);
+  useInterval(moveSnake, gameSpeed);
   return (
-    gameOver ? 
-      <h1>Game Over!</h1> : 
-      <div>
-        <div className="Grid">
-          <Grid snake={snake} food={food} />
-        </div>
+    <div>
+      <h1 className="Score">Score: {score}</h1>
+      <div className="Grid">
+        <Grid snake={snake} food={food} />
+      </div>
+      <button 
+        type="button" 
+        className="btn btn-primary"
+        onClick={() => setGameSpeed(300)}
+      >
+        Start
+      </button>
+      <button 
+        type="button" 
+        className="btn btn-primary"
+        onClick={reset}
+      >
+        Reset
+      </button>
+      <div className="Movements">
+        <button 
+          id="Up"
+          type="button" 
+          className="btn btn-primary"
+          onClick={() => setDirection('up')}
+        >
+          Up
+        </button>
         <button 
           type="button" 
           className="btn btn-primary"
-          onClick={() => setGameStarted(200)}
+          onClick={() => setDirection('left')}
         >
-          Start
+          Left
         </button>
-        <div className="Movements">
-          <button 
-            id="Up"
-            type="button" 
-            className="btn btn-primary"
-            onClick={() => setDirection('up')}
-          >
-            Up
-          </button>
-          <button 
-            type="button" 
-            className="btn btn-primary"
-            onClick={() => setDirection('left')}
-          >
-            Left
-          </button>
-          <button 
-            type="button" 
-            className="btn btn-primary"
-            onClick={() => setDirection('down')}
-          >
-            Down
-          </button>
-          <button 
-            type="button" 
-            className="btn btn-primary"
-            onClick={() => setDirection('right')}
-          >
-            Right
-          </button>
-        </div>
+        <button 
+          type="button" 
+          className="btn btn-primary"
+          onClick={() => setDirection('down')}
+        >
+          Down
+        </button>
+        <button 
+          type="button" 
+          className="btn btn-primary"
+          onClick={() => setDirection('right')}
+        >
+          Right
+        </button>
       </div>
+    </div>
   )
 }
 
